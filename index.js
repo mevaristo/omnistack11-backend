852ded72e2e5aab7eae9359c2974021e96d39e9a
matheus-evaristo <matheus.evaristo@protonmail.com>
2020-03-23 23:41:38 -0300

    hello, world!

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const api = express();
+
+api.get('/', (request, response) => {
+    return response.json( {
+        cause: "end world disease",
+        dummy: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, ",
+            "sed do eiusmod tempor incididunt ut labore et dolore magna ",
+            "aliqua. Ut enim ad minim veniam, quis nostrud exercitation ",
+            "ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+            "Duis aute irure dolor in reprehenderit in voluptate velit ",
+            "esse cillum dolore eu fugiat nulla pariatur. Excepteur ",
+            "sint occaecat cupidatat non proident, sunt in culpa qui ",
+            "officia deserunt mollit anim id est laborum."
+        ]
+    } );
+});
+
+api.listen(3333)
