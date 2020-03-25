103893a9a6bc5e69b5371449c311764b2303bcc1
matheus-evaristo <matheus.evaristo@protonmail.com>
2020-03-24 23:13:58 -0300

    incident delete

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -24,5 +24,25 @@ module.exports = {
         const ongs = await connection('incidents').select('*');
 
         return response.json(ongs);
+    },
+
+    async delete(request, response) {
+        const { id } = request.params;
+        const ong_id = request.headers.authorization;
+
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first();
+        
+        if (incident.ong_id !== ong_id) {
+            return response.status(401).json({ error: 'Operation not permitted.' });
+        }
+
+        await connection('incidents')
+            .where('id', id)
+            .delete();
+
+        return response.status(204).send();
     }
 }
